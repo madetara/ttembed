ccec98a561d141cd13031cce8ee7a31ad9da464e
Danil V. Kabanov-Ashcheulov <x@madetara.dev>
2023-11-19 16:14:15 +0300

    refactor: move bot code to module

---
+++ src/core/bot.rs
@@ -0,0 +1,124 @@
+use std::{collections::HashSet, env};
+
+use anyhow::Result;
+use teloxide::{prelude::*, types::InputFile, update_listeners::webhooks};
+use tokio::fs;
+use url::Url;
+
+use crate::core::downloader::{generic::GenericDownloader, Downloader};
+
+const MAX_SIZE: u64 = 50 * 1024 * 1024;
+
+pub async fn run() -> Result<()> {
+    let token = env::var("TG_TOKEN").expect("Telegram token not found");
+    let bot = Bot::new(token);
+
+    let bot_url = env::var("BOT_URL")
+        .expect("BOT_URL not set")
+        .parse()
+        .expect("BOT_URL is in incorrect format");
+
+    let bot_port = env::var("BOT_PORT")
+        .expect("BOT_PORT not set")
+        .parse::<u16>()
+        .expect("BOT_PORT is not a number");
+
+    let listener = webhooks::axum(
+        bot.clone(),
+        webhooks::Options::new(([0, 0, 0, 0], bot_port).into(), bot_url),
+    )
+    .await
+    .expect("Webhook creation failed");
+
+    teloxide::repl_with_listener(
+        bot,
+        |bot: Bot, msg: Message| async move {
+            let chat_id = msg.chat.id;
+            log::info!("received message from {chat_id}");
+
+            handle_message(&bot, &msg).await;
+
+            Ok(())
+        },
+        listener,
+    )
+    .await;
+
+    Ok(())
+}
+
+async fn handle_message(bot: &Bot, msg: &Message) {
+    if let Some(text) = msg.text() {
+        for url in get_valid_links(text) {
+            log::info!("attempting to download video from {url}");
+            match GenericDownloader::download(&url) {
+                Ok(filename) => {
+                    match fs::metadata(&filename).await {
+                        Ok(metadata) => {
+                            if metadata.len() <= MAX_SIZE {
+                                match bot
+                                    .send_video(msg.chat.id, InputFile::file(&filename))
+                                    .reply_to_message_id(msg.id)
+                                    .await
+                                {
+                                    Ok(_) => {}
+                                    Err(err) => {
+                                        log::error!("failed to send video. error: {err}")
+                                    }
+                                }
+                            }
+                        }
+                        Err(err) => {
+                            log::warn!("failed to get metadata for file {filename}. error: {err}");
+                        }
+                    }
+
+                    match fs::remove_file(&filename).await.err() {
+                        Some(err) => {
+                            log::error!("failed to delete file. message: {err}");
+                        }
+                        None => {
+                            log::info!("deleted file {filename}")
+                        }
+                    }
+                }
+                Err(err) => {
+                    log::warn!("error occurred while downloading {url}. error: {err}");
+                }
+            }
+        }
+    }
+}
+
+fn get_valid_links(text: &str) -> Vec<Url> {
+    lazy_static! {
+        static ref ALLOWED_DOMAINS: HashSet<&'static str> = HashSet::from([
+            "www.tiktok.com",
+            "vt.tiktok.com",
+            "vm.tiktok.com",
+            "tiktok.com",
+            "youtube.com",
+            "youtu.be"
+        ]);
+    }
+
+    log::info!("looking for links");
+    let mut result = vec![];
+
+    for word in text.split(' ') {
+        if let Ok(url) = Url::parse(word) {
+            if let Some(domain) = url.domain() {
+                if !ALLOWED_DOMAINS.contains(domain) {
+                    continue;
+                }
+
+                result.push(url);
+            }
+        }
+    }
+
+    let link_count = result.len();
+    log::info!("found {link_count} valid links");
+
+    result
+}
--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,13 @@
-use lazy_static::lazy_static;
-use std::collections::HashSet;
-use std::env;
-use std::fs;
-use teloxide::payloads::SendVideoSetters;
-use teloxide::prelude2::*;
-use teloxide::types::InputFile;
-use url::Url;
+#[macro_use]
+extern crate lazy_static;
 
-mod downloader;
-
-use downloader::generic::GenericDownloader;
-use downloader::Downloader;
-
-const MAX_SIZE: u64 = 50 * 1024 * 1024;
+mod core;
 
 #[tokio::main]
 async fn main() {
-    teloxide::enable_logging!();
+    pretty_env_logger::init();
 
     log::info!("Starting...");
 
-    let token = env::var("TG_TOKEN").expect("Telegram token not found");
-
-    let bot = Bot::new(token).auto_send();
-
-    teloxide::repls2::repl(
-        bot.clone(),
-        |message: Message, bot: AutoSend<Bot>| async move {
-            let chat_id = message.chat.id;
-            log::info!("received message from {chat_id}");
-            if let Some(text) = message.text() {
-                for url in get_valid_links(text) {
-                    log::info!("attempting to download video from {url}");
-                    match GenericDownloader::download(&url) {
-                        Ok(filename) => {
-                            match fs::metadata(&filename) {
-                                Ok(metadata) => {
-                                    if metadata.len() < MAX_SIZE {
-                                        bot.send_video(message.chat.id, InputFile::file(&filename))
-                                            .reply_to_message_id(message.id)
-                                            .await?;
-                                    }
-                                }
-                                Err(err) => {
-                                    log::warn!(
-                                        "failed to get metadata for file {filename}. error: {err}"
-                                    );
-                                }
-                            }
-
-                            match fs::remove_file(&filename).err() {
-                                Some(err) => {
-                                    log::error!("failed to delete file. message: {err}");
-                                }
-                                None => {
-                                    log::info!("deleted file {filename}")
-                                }
-                            }
-                        }
-                        Err(err) => {
-                            log::warn!("error occurred while downloading {url}. error: {err}");
-                        }
-                    }
-                }
-            }
-
-            respond(())
-        },
-    )
-        .await;
-}
-
-fn get_valid_links(text: &str) -> Vec<Url> {
-    lazy_static! {
-        static ref ALLOWED_DOMAINS: HashSet<&'static str> =
-            HashSet::from([
-            "www.tiktok.com",
-            "vt.tiktok.com",
-            "vm.tiktok.com",
-            "tiktok.com",
-            "youtube.com",
-            "youtu.be"]);
-    }
-
-    log::info!("looking for links");
-    let mut result = vec![];
-
-    for word in text.split(' ') {
-        if let Ok(url) = Url::parse(word) {
-            if let Some(domain) = url.domain() {
-                if !ALLOWED_DOMAINS.contains(domain) {
-                    continue;
-                }
-
-                result.push(url);
-            }
-        }
-    }
-
-    let link_count = result.len();
-    log::info!("found {link_count} valid links");
-
-    result
+    core::bot::run().await.unwrap();
 }
